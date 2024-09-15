f01f4c7f3fa702a5c19087612038b22223beb3f8
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-14 20:45:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -24,3 +24,25 @@ def restar():
         label_resultado.config(text=f"Resultado: {resultado}")
     except ValueError:
         messagebox.showerror("Error", "Por favor ingresa números válidos")
+
+
+def multiplicar():
+    try:
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
+        resultado = num1 * num2
+        label_resultado.config(text=f"Resultado: {resultado}")
+    except ValueError:
+        messagebox.showerror("Error", "Por favor ingresa números válidos")
+
+def dividir():
+    try:
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
+        if num2 == 0:
+            messagebox.showerror("Error", "No se puede dividir por cero")
+        else:
+            resultado = num1 / num2
+            label_resultado.config(text=f"Resultado: {resultado}")
+    except ValueError:
+        messagebox.showerror("Error", "Por f
